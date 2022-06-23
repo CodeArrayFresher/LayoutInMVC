8726c92e767d2b934d5230c27972fbcb2da9460c
Siddhesh Kumbhar <siddhu.kumbhar2000@gmail.com>
2022-06-23 11:48:38 +0530

    test commit

--- LayoutInMVC/App_Start/RouteConfig.cs
+++ LayoutInMVC/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace LayoutInMVC
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //routes.MapMvcAttributeRoutes();
             routes.MapMvcAttributeRoutes();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
